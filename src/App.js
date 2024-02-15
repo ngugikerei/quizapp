7325f323fbd4c472ce5cf2ca7abd516fda48d59c
Rununu Games <ngugikerei@gmail.com>
2024-02-15 15:20:24 +0300

    useReducer fetch data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,70 @@
-import DateCounter from './DateCounter';
+import { useEffect, useReducer, useState } from 'react';
+import Header from './Header';
+import Loader from './Loader';
+import Error from './Error';
+import Main from './Main';
+import StartScreen from './StartScreen';
+
+const initialState = {
+  questions: [],
+  // "loading" , "error", 'ready' , 'active', 'complete
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        status: 'ready',
+        questions: action.payload,
+      };
+    case 'dataFetching':
+      return {
+        ...state,
+        status: 'loading',
+      };
+
+    case 'dataFailed':
+      return {
+        ...state,
+        status: 'error',
+      };
+
+    default:
+      throw new Error('Action unknown');
+  }
+}
 
 export default function App() {
-  return <div className="app"></div>;
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { status, questions } = state;
+
+  const numQuestions = questions.length;
+
+  //fetch data from local API
+  useEffect(function () {
+    async function fetchData() {
+      try {
+        const res = await fetch('http://localhost:8000/questions');
+        const data = await res.json();
+        dispatch({ type: 'dataReceived', payload: data });
+      } catch (error) {
+        dispatch({ type: 'dataFailed' });
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        {status === 'loading' && <Loader />}
+        {status === 'error' && <Error />}
+        {status === 'ready' && <StartScreen numQuestions={numQuestions} />}
+      </Main>
+    </div>
+  );
 }
